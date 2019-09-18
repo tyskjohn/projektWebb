6fd6537ce28de495ba0d7a0a3bb0ab1eff5116fc
Samuel Brinkeback <samuel.brinkeback@hotmail.com>
2019-09-18 09:33:05 +0200

    update

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ProductCollageComponent } from './product-collage/product-collage.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProductCollageComponent
   ],
   imports: [
     BrowserModule,
