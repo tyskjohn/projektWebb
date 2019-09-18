99f66822673eab6e846f184d40ae9a8ee28c247f
Johan <johan.tysk@hotmail.se>
2019-09-18 15:05:24 +0200

    profile added

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { HomeComponent } from './home/home.component';
+import { ProfileComponent } from './profile/profile.component';
 
 const routes: Routes = [
 
   { path: '', component: HomeComponent },
+
+
+  { path: 'profile', component: ProfileComponent },
 
 ];
 
