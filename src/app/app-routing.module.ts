7175bbf329ac147fcf9806133e8c72dcd7f7f848
Johan <johan.tysk@hotmail.se>
2019-09-18 09:35:58 +0200

    home added

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { NewsletterComponent } from './newsletter/newsletter.component';
+import { HomeComponent } from './home/home.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+
+  { path: '', component: HomeComponent },
+  { path: 'newsletter', component: NewsletterComponent },
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NewsletterComponent } from './newsletter/newsletter.component';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NewsletterComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
