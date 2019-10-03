d37a270f5ce7a377275ab5dd8e1a4d743071c644
Sami <sami-weso@hotmail.com>
2019-10-03 11:11:44 +0200

    auth servic

--- src/app/auth.service.ts
+++ src/app/auth.service.ts
@@ -1,9 +1,32 @@
 import { Injectable } from '@angular/core';
+//import { User } from './user';
+import { HttpClient } from '@angular/common/http'
+//import { Observable } from 'rxjs'
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
+  __apiUrl: string = "http://localhost:3001/api";
 
-  constructor() { }
+   constructor(private http: HttpClient) { }
+
+   public login(userInfo: User) {
+    return this.http.post(`${this.__apiUrl}/users/login`, userInfo);
+  }
+
+  public register(userInfo: User) {
+    return this.http.post(`${this.__apiUrl}/users/register`, userInfo);
+  }
+}
+export interface User {
+  _id: string;
+  firstname: string;
+  lastname: string;
+  email: string;
+  password: string;
+  addressline: string;
+  city: string;
+  country: string;
+  zipcode: string;
 }
--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { AuthService } from '../auth.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-register',
@@ -7,9 +10,48 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RegisterComponent implements OnInit {
 
-  constructor() { }
+  constructor(private authService: AuthService, private router: Router, private formBuilder: FormBuilder) { }
+
+  registerForm: FormGroup;
+  isSubmitted: boolean = false;
+
 
   ngOnInit() {
+    this.registerForm = this.formBuilder.group({
+      firstname: ['', Validators.required],
+      lastname: ['', Validators.required],
+      addressline: ['', Validators.required],
+      zipcode: ['', Validators.required],
+      city: ['', Validators.required],
+      country: ['', Validators.required],
+      email: ['', Validators.required],
+      password: ['', Validators.required]
+    })
   }
 
+
+  
+  get formControls() { return this.registerForm.controls }
+
+
+  register() {
+    this.isSubmitted = true;
+
+    if(this.registerForm.invalid) {
+      return;
+    }
+
+    this.authService.register(this.registerForm.value).subscribe((registerres) => {
+      if(registerres["success"]) {
+        this.authService.login(this.registerForm.value).subscribe((loginres) => {
+          if(loginres["success"]) {
+            this.router.navigateByUrl('/login');
+          }
+          else {
+            this.router.navigateByUrl('/register');
+          }         
+        })        
+      }    
+    })
+  }
 }
