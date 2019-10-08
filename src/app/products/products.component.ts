a2b12ef8dcdd4dc0eb797e699d7a4939ca1b77ac
Johan <johan.tysk@hotmail.se>
2019-10-08 19:46:50 +0200

    shopping cart added, checkout prodcucts added

--- src/app/products/products.component.ts
+++ src/app/products/products.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { ProductService } from '../product.service';
+import { Product } from '../../app/entities/product.entity';
 
 @Component({
   selector: 'app-products',
@@ -7,9 +9,14 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProductsComponent implements OnInit {
 
-  constructor() { }
+  private products: Product[];
+
+  constructor(private productService: ProductService) { }
 
   ngOnInit() {
+
+    this.products = this.productService.getAllProducts();
+
   }
 
 }
